cb11f5f5c0b6ec74fcca6c47c4cd4b7e6bacc30e
JoesMac <joesmac@JoesMacs-Mac.local>
2018-06-28 14:43:12 -0700

    custom tip

--- TippyRedux/ViewController.swift
+++ TippyRedux/ViewController.swift
@@ -15,11 +15,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var customField: UITextField!
+    //var customTip = 0.0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        customField.isHidden = true
         view.setGradientBackground(colorOne: .black, colorTwo: .purple)
     }
 
@@ -32,12 +35,36 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
+    @IBAction func segmentTap(_ sender: Any) {
+        let getIndex = tipControl.selectedSegmentIndex
+        if(getIndex == 3){
+            print("Custom")
+            
+        }
+        switch(getIndex){
+        case 0:
+            customField.isHidden = true
+        case 1:
+            customField.isHidden = true
+        case 2:
+            customField.isHidden = true
+        case 3:
+            customField.isHidden = false
+        default:
+            customField.isHidden = true
+
+        }
+        
+    }
     @IBAction func calculateTip(_ sender: Any) {
+        let customTip = Double(customField.text!) ?? 0
+        let tipPercentages = [0.1, 0.15, 0.2, customTip]
+       
         let bill = Double(billField.text!) ?? 0
-        let tip = bill * 0.2
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
-        tipLabel.text = "$\(tip)"
-        totalLabel.text = "$\(total)"
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
 }
 
